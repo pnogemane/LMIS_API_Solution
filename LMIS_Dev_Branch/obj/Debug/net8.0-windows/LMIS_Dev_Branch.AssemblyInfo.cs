c3fc16048c98a23030e3c98bfabedcff38cf8410
littlepigsoftwaredevelopment <phiwokuhle.nogemane@gmail.com>
2024-11-12 21:58:12 +0200

    Adding Dapper Functions

--- LMIS_Dev_Branch/obj/Debug/net8.0-windows/LMIS_Dev_Branch.AssemblyInfo.cs
+++ LMIS_Dev_Branch/obj/Debug/net8.0-windows/LMIS_Dev_Branch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8b010d2679a9625f9d985de7326ca97fd946b7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac49724fcb947fdb7bdedb0547915ecd82c6443")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMIS_Dev_Branch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
